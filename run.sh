322ea8b4dec2d3a28439743571944fa2901c0492
Chris <chris@outlook.com>
2024-11-08 12:08:58 -0800

    change script

--- run.sh
+++ run.sh
@@ -6,8 +6,14 @@ nx=250
 ny=250
 niter=1000
 make $mod
-srun -N 2 ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
-# python utils/visualizer.py ${mod}.out ${mod}.gif
+
+if [ "$mod" = "mpi" ]; then
+  srun -N 2 -n 1 ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
+else
+  ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
+fi
+
+python utils/visualizer.py ${mod}.out ${mod}.gif
 
 # ncu --target-processes all --export report_${mod}.ncu-rep ./build/${mod}
 # ncu --import report_${mod}.ncu-rep > report_${mod}.txt
