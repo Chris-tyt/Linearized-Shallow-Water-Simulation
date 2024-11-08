3000dcf8f94cae2f29d944f40ffb18eea5c35d90
Chris <chris@outlook.com>
2024-11-08 13:59:18 -0800

    final mpi

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # option: mpi gpu serial serial_omp basic_serial 
 # --scenario water_drop dam_break wave river
 mod=mpi
-scenario=water_drop
+scenario=dam_break
 nx=250
 ny=250
 niter=1000
 make $mod
 
 if [ "$mod" = "mpi" ]; then
-  srun -N 2 -n 1 ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
+  srun -N 3 -n 9 ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
 else
   ./build/${mod} --nx ${nx} --ny ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
 fi
